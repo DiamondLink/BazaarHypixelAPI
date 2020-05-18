a3b829433e1b1997e914d180d1401e4971f19ad2
DiamondLink <59293428+DiamondLink@users.noreply.github.com>
2020-05-18 21:05:22 +0200

    Update Main.java

--- src/main/java/com/bazaarapi/diamondlink/Main.java
+++ src/main/java/com/bazaarapi/diamondlink/Main.java
@@ -2,6 +2,6 @@ package com.bazaarapi.diamondlink;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("\nYou must import this api in order to use it : \n\nimport com.bazaarapi.diamondlink.api.BazaarAPI;\nimport com.bazaarapi.diamondlink.modules.BazaarAPIReader;\nimport org.json.*;\nBazaarAPIReader bazaarAPIReader = BazaarAPI.getBazaarAPIReader();\n\nHave a look at the README.md for more info\n");
+		System.out.println("\nYou must import this api in order to use it ! See the README.md for instructions");
 	}
 }
