f18a3cda4d8049a6cd8b6d3c8267e8ed2ecf9a64
DiamondLink <adressepourgithub@gmail.com>
2020-06-09 22:36:35 +0200

    Fixed wrong urls, containing "

--- src/main/java/com/bazaarapi/diamondlink/utils/APIURLsUtils.java
+++ src/main/java/com/bazaarapi/diamondlink/utils/APIURLsUtils.java
@@ -3,18 +3,18 @@ package com.bazaarapi.diamondlink.utils;
 public class APIURLsUtils {
 	
 	public static String getBazaarProductsAPIURL(String apiKey) {
-		return "https://api.hypixel.net/skyblock/bazaar/products?key=" + apiKey;
+		return ("https://api.hypixel.net/skyblock/bazaar/products?key=" + apiKey).replace("\"", "");
 	}
 	
 	public static String getBazaarDataAPIURl(String productID, String apiKey) {
-		return "https://api.hypixel.net/skyblock/bazaar/product?key=" + apiKey + "&productId=" + productID;
+		return ("https://api.hypixel.net/skyblock/bazaar/product?key=" + apiKey + "&productId=" + productID).replace("\"", "");
 	}
 	
 	public static String getPlayerStatsURL(String uuid, String apiKey) {
-		return "https://api.hypixel.net/player?key=" + apiKey + "&uuid=" + uuid;
+		return ("https://api.hypixel.net/player?key=" + apiKey + "&uuid=" + uuid).replace("\"", "");
 	}
 	
 	public static String getSkyblockStats(String profileID, String apiKey) {
-		return "https://api.hypixel.net/skyblock/profile?key=" + apiKey + "&profile=" + profileID;
+		return ("https://api.hypixel.net/skyblock/profile?key=" + apiKey + "&profile=" + profileID).replace("\"", "");
 	}
 }
